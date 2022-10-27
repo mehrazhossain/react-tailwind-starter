6b6ac25bfa08afa2941fca1dea7cc4256f0d393b
Mehraz Hossain <mehrazhossain54@gmail.com>
2022-10-27 21:53:44 +0600

    configured Tailwind

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,7 +1,9 @@
 const Greet = () => {
   return (
     <div>
-      <p>Hello, world!</p>
+      <h1 className="text-3xl font-bold text-purple-700 underline">
+        Hello world!
+      </h1>
     </div>
   );
 };
